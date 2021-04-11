dcd38cd3e7f8b0d178ee91eafd08907fb169cef8
Kesley David <kesleygti@gmail.com>
2021-04-11 18:31:06 -0300

    refactor: change link to new server backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/KesleyDavid/challenge-ton-react-native',
+  baseURL: 'https://api.kesley.dev/ton',
 });
 
 export default api;
